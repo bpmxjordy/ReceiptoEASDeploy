b4ce56c3ffece097863938f1910f488459f1e4ab
Jordan Cartwright <48060710+bpmxjordy@users.noreply.github.com>
2023-12-13 23:50:45 +0000

    updated receipt detail view ui

--- Screens/ReceiptDetailScreen.js
+++ Screens/ReceiptDetailScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, Text, View, StyleSheet, ActivityIndicator, TouchableOpacity} from 'react-native';
+import { ScrollView , Text, View, StyleSheet} from 'react-native';
 import { useAuth } from '../AuthContext';
 import ReadReceipt from './ReadReceipt';
 
@@ -31,13 +31,14 @@ const ReceiptDetailScreen = ({ route }) => {
             <Text style={{color: 'white', fontSize: 15, fontWeight: 'bold'}}>Time: {time}</Text>
             <Text style={{color: 'white', fontSize: 20, marginTop: '5%', fontWeight: 'bold'}}>Total Amount: £{receipt.totalAmount}</Text>
             <Text style={{color: 'white', marginTop: '5%', fontSize: 25, fontWeight: 'bold'}}>Items</Text>
-            
+            <ScrollView >
             {receipt.items && receipt.items.map((item, index) => (
                 <View style={styles.items} key={index}>
-                    <View style={{width:  '60%', height: 'auto'}}><Text style={styles.textTitle}>{item.name}</Text></View>
-                    <View style={{width: 'auto', height: 'auto'}}><Text style={styles.textPrice}>Price: £{item.price} </Text></View>
+                    <View style={{width:  '85%', height: 'auto'}}><Text style={styles.textTitle}>{item.name}</Text><Text style={styles.textCategory}>{item.category}</Text></View>
+                    <View style={{width: 'auto', height: 'auto'}}><Text style={styles.textPrice}>£{item.price} </Text></View>
                 </View>
             ))}
+            </ScrollView>
             
         </View>
     );
@@ -51,6 +52,9 @@ const styles = StyleSheet.create({
         padding: 20,
         backgroundColor: '#080B16',
     },
+    scrollView: {
+        
+    },
     items: {
         width: '100%',
         height: '10%',
@@ -63,17 +67,23 @@ const styles = StyleSheet.create({
     },
     textTitle: {
         width: 'auto',
-        
+        fontSize: 20,
+        fontWeight: 'bold',
         textAlign: 'left',
         color: 'white',
         marginLeft: '10%'
     },
     textPrice: {
-        width: 'auto',
+        width: '100%',
         textAlign: 'right',
-        color: 'white',
+        color: 'white'
+    },textCategory: {
+        width: 'auto',
+        fontStyle: 'italic',
+        textAlign: 'left',
+        color: '#646978',
         marginLeft: '10%'
-    }
+    },
 });
 
 export default ReceiptDetailScreen;
