892ae280cf60b25b1f03a6af3284f14923098e3d
Jordan Cartwright <48060710+bpmxjordy@users.noreply.github.com>
2025-02-15 22:50:34 +0000

    Updating before build

--- android/app/src/main/java/com/Receipto/receiptoapp/MainActivity.kt
+++ android/app/src/main/java/com/Receipto/receiptoapp/MainActivity.kt
@@ -8,6 +8,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.Receipto.receiptoapp.BuildConfig
 
 import expo.modules.ReactActivityDelegateWrapper
 
@@ -27,7 +28,9 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "main"
+  override fun getMainComponentName(): String? {
+        return "ReceiptoApplication"
+    }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
